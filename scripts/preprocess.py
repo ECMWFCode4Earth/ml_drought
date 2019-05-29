e9dc0ebc24673384fe1f7cc49926747d00fd2540
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-05-29 10:45:59 -0400

    Add preprocessing script with regridding

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -17,17 +17,21 @@ def process_precip_2018():
                          regrid=None,
                          parallel=False)
 
+
 def process_vhi_2018():
     # if the working directory is alread ml_drought don't need ../data
     if Path('.').absolute().as_posix().split('/')[-1] == 'ml_drought':
         data_path = Path('data')
     else:
         data_path = Path('../data')
+    regrid_path = data_path / 'interim/chirps/chirps-v2.0.monthly_kenya.nc'
+    assert regrid_path.exists(), f'{regrid_path} not available'
 
     processor = VHIPreprocessor(data_path)
 
-
+    processor.preprocess(subset_kenya=True, regrid=regrid_path,
+                         parallel=False, resample_time='M', upsampling=False)
 
 
 if __name__ == '__main__':
-    process_precip_2018()
+    process_vhi_2018()
