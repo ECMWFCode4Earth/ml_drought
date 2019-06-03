7ae4c1ac66e4d2d040ef66377cf3334d43d8a702
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-06-03 15:09:03 -0400

    Test y as well

--- tests/models/test_base.py
+++ tests/models/test_base.py
@@ -30,5 +30,10 @@ class TestBase:
                 target = x.isel(time=time).sel(lat=lat).sel(lon=lon).VHI.values
 
                 assert target == x_np[idx, time, 0], \
-                    f'Got different values for time idx: {time}, lat: {lat}, ' \
+                    f'Got different x values for time idx: {time}, lat: {lat}, ' \
                     f'lon: {lon}.Expected {target}, got {x_np[idx, time, 0]}'
+
+            target_y = y.isel(time=0).sel(lat=lat).sel(lon=lon).VHI.values
+            assert target_y == y_np[idx, 0], \
+                f'Got y different values for lat: {lat}, ' \
+                f'lon: {lon}.Expected {target_y}, got {y_np[idx, 0]}'
