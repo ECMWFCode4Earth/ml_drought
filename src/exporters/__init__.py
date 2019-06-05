c01b8d33f7a92319a749d4ec7510389c2fb10577
tommylees112 <thomas.lees112@gmail.com>
2019-06-05 22:41:13 +0100

    fixthe changes with the init

--- src/exporters/__init__.py
+++ src/exporters/__init__.py
@@ -2,7 +2,7 @@ from .cds import ERA5Exporter
 from .vhi import VHIExporter
 from .chirps import CHIRPSExporter
 from .planetOS import ERA5ExporterPOS
-from .s5.seas5 import S5Exporter
+from .s5.seas5.s5 import S5Exporter
 
 __all__ = [
     'ERA5Exporter', 'VHIExporter', 'ERA5ExporterPOS',
