152f533030e7bf59fa040559dd783688cb1f164c
tommylees112 <thomas.lees112@gmail.com>
2019-06-11 17:28:22 +0100

    initial tests added

--- tests/engineer/test_nowcast.py
+++ tests/engineer/test_nowcast.py
@@ -1,6 +1,6 @@
 import pytest
 # import pickle
-# import numpy as np
+import numpy as np
 # import xarray as xr
 
 from src.engineer import Nowcast
@@ -24,4 +24,15 @@ class TestNowcast(TestEngineer):
         assert (tmp_path / 'features' / 'nowcast').exists(), '\
         nowcast directory not made!'
 
-    pass
+    def test_yearsplit(self, tmp_path):
+
+        self._setup(tmp_path)
+
+        dataset, _, _ = _make_dataset(size=(2, 2))
+
+        engineer = Nowcast(tmp_path)
+        train = engineer._train_test_split(dataset, years=[2001],
+                                           target_variable='VHI')
+
+        assert (train.time.values < np.datetime64('2001-01-01')).all(), \
+            'Got years greater than the test year in the training set!'
