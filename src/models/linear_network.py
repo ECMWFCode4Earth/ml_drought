2680da00bccb2c68687426c375f0a44b8332fdb7
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-06-11 09:35:16 -0400

    Add train function

--- src/models/linear_network.py
+++ src/models/linear_network.py
@@ -1,9 +1,14 @@
-from torch import nn
+import numpy as np
 from pathlib import Path
 
-from typing import cast, List, Union
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+from typing import cast, List, Optional, Union
 
 from .base import ModelBase
+from .data import DataLoader, train_val_mask
 
 
 class LinearNetwork(ModelBase):
@@ -19,6 +24,80 @@ class LinearNetwork(ModelBase):
 
         self.layer_sizes = layer_sizes
         self.dropout = dropout
+
+    def train(self, num_epochs: int = 1,
+              early_stopping: Optional[int] = None,
+              batch_size: int = 256,
+              learning_rate: float = 1e-3) -> None:
+        print(f'Training {self.model_name}')
+
+        if early_stopping is not None:
+            len_mask = len(DataLoader._load_datasets(self.data_path, mode='train',
+                                                     shuffle_data=False))
+            train_mask, val_mask = train_val_mask(len_mask, 0.3)
+
+            train_dataloader = DataLoader(data_path=self.data_path,
+                                          batch_file_size=self.batch_size,
+                                          shuffle_data=True, mode='train', mask=train_mask,
+                                          to_tensor=True)
+            val_dataloader = DataLoader(data_path=self.data_path,
+                                        batch_file_size=self.batch_size,
+                                        shuffle_data=False, mode='train', mask=val_mask,
+                                        to_tensor=True)
+            batches_without_improvement = 0
+            best_val_score = np.inf
+        else:
+            train_dataloader = DataLoader(data_path=self.data_path,
+                                          batch_file_size=self.batch_size,
+                                          shuffle_data=True, mode='train',
+                                          to_tensor=True)
+
+        # initialize the model
+        x_ref, _ = next(iter(train_dataloader))
+        input_size = x_ref.view(x_ref.shape[0], -1)
+        self.model: LinearModel = LinearModel(input_size=input_size[1],
+                                              layer_sizes=self.layer_sizes,
+                                              dropout=self.dropout)
+
+        optimizer = torch.optim.Adam([pam for pam in self.model.parameters()],
+                                     lr=learning_rate)
+
+        for epoch in range(num_epochs):
+            train_rmse = []
+            self.model.train()
+            for x, y in train_dataloader:
+                # TODO: break x and y into more batches
+                optimizer.zero_grad()
+                pred = self.model(x)
+                loss = F.smooth_l1_loss(pred, y)
+                loss.backward()
+                optimizer.step()
+
+                train_rmse.append(loss.item())
+
+            if early_stopping is not None:
+                self.model.eval()
+                val_rmse = []
+                with torch.no_grad():
+                    for x, y in val_dataloader:
+                        val_pred_y = self.model(x)
+                        val_loss = F.mse_loss(val_pred_y, y)
+
+                        val_rmse.append(val_loss.item())
+
+            print(f'Epoch {epoch + 1}, train RMSE: {np.mean(train_rmse)}')
+
+            if early_stopping is not None:
+                epoch_val_rmse = np.mean(val_rmse)
+                print(f'Val RMSE: {epoch_val_rmse}')
+                if epoch_val_rmse < best_val_score:
+                    batches_without_improvement = 0
+                    best_val_score = epoch_val_rmse
+                else:
+                    batches_without_improvement += 1
+                    if batches_without_improvement == early_stopping:
+                        print('Early stopping!')
+                        return None
 
 
 class LinearModel(nn.Module):
