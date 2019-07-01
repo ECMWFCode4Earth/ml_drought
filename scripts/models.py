158b96288738eb63f51abd5bf60b84332b4fbd5a
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-07-01 09:08:11 -0700

    Add shap values to the linear network script

--- scripts/models.py
+++ scripts/models.py
@@ -68,8 +68,31 @@ def linear_nn():
     predictor.train()
     predictor.evaluate(save_preds=True)
 
+    # mostly to test it works
+    test_arrays_loader = DataLoader(data_path=data_path, batch_file_size=1,
+                                    shuffle_data=False, mode='test', to_tensor=True)
+    key, val = list(next(iter(test_arrays_loader)).items())[0]
+
+    explanations = predictor.explain(val.x[:3])
+    print(explanations.shape)
+    np.save('shap_linear_network.npy', explanations)
+    np.save('shap_x_linear_network.npy', val.x[:3])
+
+    with open('variables_linear_network.txt', 'w') as f:
+        f.write(str(val.x_vars))
+
+    # plot the variables
+    with (data_path / 'features/normalizing_dict.pkl').open('rb') as f:
+        normalizing_dict = pickle.load(f)
+
+    for variable in val.x_vars:
+        plt.clf()
+        plot_shap_values(val.x[0].numpy(), explanations[0], val.x_vars, normalizing_dict, variable,
+                         normalize_shap_plots=True, show=False)
+        plt.savefig(f'{variable}_linear_network.png', dpi=300, bbox_inches='tight')
+
 
 if __name__ == '__main__':
-    # parsimonious()
+    parsimonious()
     regression()
-    # linear_nn()
+    linear_nn()
