ad8e52b62fd0ce44943ef213f5c9e0366b9ab146
tommylees112 <thomas.lees112@gmail.com>
2019-07-08 10:44:27 +0100

    remove excess preprocess functions

--- src/preprocess/seas5/s5.py
+++ src/preprocess/seas5/s5.py
@@ -37,8 +37,8 @@ class S5Preprocessor(BasePreProcessor):
         ds = xr.open_dataset(filepath, engine='cfgrib')
 
         ds = ds.rename({
-            'time': 'initialisation_date', 'step':'forecast_horizon',
-            'valid_time':'time'
+            'time': 'initialisation_date', 'step': 'forecast_horizon',
+            'valid_time': 'time'
         })
         if ds.surface.values.size == 1:
             ds = ds.drop('surface')
@@ -56,66 +56,6 @@ class S5Preprocessor(BasePreProcessor):
         filename = filepath.stem + f'_{variable}{subset_name}.nc'
         output_path = output_dir / filename
         return output_path
-
-    def _preprocess_one_var(self, ds: xr.Dataset,
-                            variable: str,
-                            filepath: Path,
-                            subset_str: Optional[str],
-                            regrid: Optional[bool] = None) -> Path:
-        # 2. subset ROI
-        if subset_str is not None:
-            try:
-                ds = self.chop_roi(ds, subset_str)
-            except:
-                ds = self.chop_roi(ds, subset_str, inverse_lat=True)
-        # 3. regrid
-        if regrid is not None:
-            ds = self.regrid(ds, regrid)
-
-        # 4. create the filepath and save to that location
-        output_path = self.create_filename(
-            filepath=filepath,
-            output_dir=self.out_dir,
-            variable=variable,
-            subset_name=subset_str if subset_str is not None else None
-        )
-        assert output_path.name[-3:] == '.nc', f'\
-        filepath name should be a .nc file. Currently: {filepath.name}'
-
-        # 5. save ds to output_path
-        ds.to_netcdf(output_path)
-
-        return output_path
-
-    def _preprocess_vars_separately(self,
-                                    filepath: Path,
-                                    ouce_server: bool = False,
-                                    regrid: Optional[xr.Dataset] = None,
-                                    subset_str: Optional[str] = None,
-                                    ) -> List[Path]:
-        """preprocess a single s5 dataset (each variable separately)"""
-        if ouce_server:
-            # undoes the preprocessing so that both are consistent
-            # 1. read nc file
-            o = OuceS5Data()
-            ds = o.read_ouce_s5_data(filepath)
-        else:  # downloaded from CDSAPI as .grib
-            # 1. read grib file
-            ds = self.read_grib_file(filepath)
-
-        # find all variables (sometimes download multiple)
-        coords = [c for c in ds.coords]
-        vars = [v for v in ds.variables if v not in coords]
-
-        output_paths = []
-        for var in vars:
-            ds_one_var = ds[var].to_dataset(name=var)
-            output_paths.append(self._preprocess_one_var(
-                ds=ds_one_var, variable=var, filepath=filepath,
-                subset_str=subset_str, regrid=regrid
-            ))
-
-        return output_paths
 
     def _preprocess(self,
                     filepath: Path,
@@ -162,7 +102,7 @@ class S5Preprocessor(BasePreProcessor):
         if subset_str is not None:
             try:
                 ds = self.chop_roi(ds, subset_str)
-            except:
+            except AssertionError:
                 ds = self.chop_roi(ds, subset_str, inverse_lat=True)
 
         # 5. regrid (one variable at a time)
