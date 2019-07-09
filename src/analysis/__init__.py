47c8b908e5bfc0de21b9da485da84dfef84fa0cb
tommylees112 <thomas.lees112@gmail.com>
2019-07-09 12:04:36 +0100

    init to pull the indices

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -1,5 +1,16 @@
 from .event_detector import EventDetector
 from .plot_shap import plot_shap_values
-from .indices import SPI
+from .indices import (
+    SPI
+    ZScoreIndex,
+    PercentNormalIndex,
+    DroughtSeverityIndex,
+    ChinaZIndex,
+    DecileIndex,
+    AnomalyIndex,
+)
 
-__all__ = ['plot_shap_values', 'EventDetector', 'SPI',]
+__all__ = ['plot_shap_values', 'EventDetector', 'SPI', 'ZScoreIndex',
+    'PercentNormalIndex', 'DroughtSeverityIndex',
+    'ChinaZIndex', 'DecileIndex', 'AnomalyIndex',
+]
--- src/analysis/indices/__init__.py
+++ src/analysis/indices/__init__.py
@@ -1,3 +1,12 @@
 from .spi import SPI
+from .z_score import ZScoreIndex
+from .percent_normal_index import PercentNormalIndex
+from .drought_severity_index import DroughtSeverityIndex
+from .china_z_index import ChinaZIndex
+from .decile_index import DecileIndex
+from .anomaly_index import AnomalyIndex
 
-__all__ = ["SPI"]
+__all__ = [
+    'SPI', 'ZScoreIndex', 'PercentNormalIndex', 'DroughtSeverityIndex',
+    'ChinaZIndex', 'DecileIndex', 'AnomalyIndex',
+]
