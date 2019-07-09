a9c2e93c55d8d736a67d939b86adfb5a86b6eea0
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-07-09 15:04:06 -0400

    Update models script

--- scripts/models.py
+++ scripts/models.py
@@ -29,11 +29,10 @@ def regression():
     else:
         data_path = Path('../data')
 
-    predictor = LinearRegression(data_path)
-    predictor.train()
+    predictor = LinearRegression(data_path, include_pred_month=True)
+    predictor.train(num_epochs=100, early_stopping=5)
     predictor.evaluate(save_preds=True)
 
-    # mostly to test it works
     test_arrays_loader = DataLoader(data_path=data_path, batch_file_size=1,
                                     shuffle_data=False, mode='test')
     key, val = list(next(iter(test_arrays_loader)).items())[0]
@@ -66,35 +65,38 @@ def linear_nn():
     else:
         data_path = Path('../data')
 
-    predictor = LinearNetwork(layer_sizes=[100], data_folder=data_path)
-    predictor.train()
+    predictor = LinearNetwork(layer_sizes=[100, 100], data_folder=data_path)
+    predictor.train(num_epochs=100, early_stopping=5)
     predictor.evaluate(save_preds=True)
 
-    # mostly to test it works
-    test_arrays_loader = DataLoader(data_path=data_path, batch_file_size=1,
-                                    shuffle_data=False, mode='test', to_tensor=True)
-    key, val = list(next(iter(test_arrays_loader)).items())[0]
+    # The code below is commented out because of a bug in the shap deep Explainer which
+    # prevents it from working. It has been fixed in master, but not deployed yet:
+    # https://github.com/slundberg/shap/pull/684
 
-    explain_hist, explain_add = predictor.explain([val.x.historical[:3], val.x.pred_months[:3]])
-    print(explain_hist.shape)
-    np.save('shap_linear_network_hist.npy', explain_hist)
-    np.save('shap_linear_network_add.npy', explain_add)
-
-    np.save('shap_x_linear_network_hist.npy', val.x.historical[:3])
-    np.save('shap_x_linear_network_add.npy', val.x.pred_months[:3])
-
-    with open('variables_linear_network.txt', 'w') as f:
-        f.write(str(val.x_vars))
-
-    # plot the variables
-    with (data_path / 'features/normalizing_dict.pkl').open('rb') as f:
-        normalizing_dict = pickle.load(f)
-
-    for variable in val.x_vars:
-        plt.clf()
-        plot_shap_values(val.x[0].numpy(), explain_hist[0], val.x_vars, normalizing_dict, variable,
-                         normalize_shap_plots=True, show=False)
-        plt.savefig(f'{variable}_linear_network.png', dpi=300, bbox_inches='tight')
+    # test_arrays_loader = DataLoader(data_path=data_path, batch_file_size=1,
+    #                                 shuffle_data=False, mode='test', to_tensor=True)
+    # key, val = list(next(iter(test_arrays_loader)).items())[0]
+    #
+    # explain_hist, explain_add = predictor.explain([val.x.historical[:3], val.x.pred_months[:3]])
+    # print(explain_hist.shape)
+    # np.save('shap_linear_network_hist.npy', explain_hist)
+    # np.save('shap_linear_network_add.npy', explain_add)
+    #
+    # np.save('shap_x_linear_network_hist.npy', val.x.historical[:3])
+    # np.save('shap_x_linear_network_add.npy', val.x.pred_months[:3])
+    #
+    # with open('variables_linear_network.txt', 'w') as f:
+    #     f.write(str(val.x_vars))
+    #
+    # # plot the variables
+    # with (data_path / 'features/normalizing_dict.pkl').open('rb') as f:
+    #     normalizing_dict = pickle.load(f)
+    #
+    # for variable in val.x_vars:
+    #     plt.clf()
+    #     plot_shap_values(val.x[0].numpy(), explain_hist[0], val.x_vars, normalizing_dict, variable,
+    #                      normalize_shap_plots=True, show=False)
+    #     plt.savefig(f'{variable}_linear_network.png', dpi=300, bbox_inches='tight')
 
 
 if __name__ == '__main__':
