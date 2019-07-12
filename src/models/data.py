8c2fcce9903d5655b7b605650152415bd2766ec4
tommylees112 <thomas.lees112@gmail.com>
2019-07-12 22:50:26 +0100

    use gabis code for getting current data

--- src/models/data.py
+++ src/models/data.py
@@ -193,30 +193,6 @@ class _BaseIter:
 
         return current
 
-    @staticmethod
-    def move_target_var_to_end_of_dataset(x: xr.Dataset, y: xr.Dataset) -> xr.Dataset:
-        # @GABI is this a sensible way to ensure we always have the `target` last?
-        # get the x_vars and target_var
-        target_var = [y for y in y.data_vars][0]
-        all_vars = [v for v in x.data_vars]
-        x_vars = [
-            x_var
-            for x_var in x.data_vars
-            if x_var != target_var
-        ]
-
-        # reorganise so that target is always last
-        new_x = x[x_vars]
-        x = xr.merge([new_x, x[target_var]])
-        new_x_vars = [x_var for x_var in x.data_vars]
-        target_var_index = int(
-            np.argwhere(np.isin(new_x_vars, [target_var]))
-        )
-        assert target_var_index == len(all_vars) - 1, f"Expect the target variable" \
-            " to be the final variable in the `x` dataset"
-
-        return x
-
     def ds_folder_to_np(self,
                         folder: Path,
                         clear_nans: bool = True,
@@ -227,12 +203,6 @@ class _BaseIter:
         x, y = xr.open_dataset(folder / 'x.nc'), xr.open_dataset(folder / 'y.nc')
         assert len(list(y.data_vars)) == 1, f'Expect only 1 target variable! ' \
             f'Got {len(list(y.data_vars))}'
-
-        if self.experiment == 'nowcast':
-            vars = [v for v in x.data_vars]
-            assert len(vars) > 1, "Must have more than target variable for nowcast"
-            # move the target variable to the final variable index
-            x = self.move_target_var_to_end_of_dataset(x=x, y=y)
 
         x_np, y_np = x.to_array().values, y.to_array().values
         if (self.normalizing_dict is not None) and (self.normalizing_array is None):
@@ -263,10 +233,9 @@ class _BaseIter:
             # if nowcast then we have a TrainData.current
             # @GABI how do we know which variable is the target_var
             historical = x_np[:, :-1, :]  # all timesteps except the final
-            current = x_np[:, -1, :-1]  # only select the NON-TARGET variables
-            current2 = self.get_current_array(x=x, y=y, x_np=x_np)
-            assert isinstance(current2, np.ndarray)
-            assert False
+            current2 = self.get_current_array(  # only select NON-TARGET vars
+                x=x, y=y, x_np=x_np
+            )
 
             train_data = TrainData(
                 current=current,
