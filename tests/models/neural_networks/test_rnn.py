b851127347019722c90f009aa075665d53ac49fe
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-07-22 08:50:24 -0400

    Print train RMSE and properly label smooth L1 loss when training neural networks (#68)

--- tests/models/neural_networks/test_rnn.py
+++ tests/models/neural_networks/test_rnn.py
@@ -78,7 +78,7 @@ class TestRecurrentNetwork:
         model.train()
 
         captured = capsys.readouterr()
-        expected_stdout = 'Epoch 1, train RMSE: 0.'
+        expected_stdout = 'Epoch 1, train smooth L1: 0.'
         assert expected_stdout in captured.out
 
         assert type(model.model) == RNN, \
