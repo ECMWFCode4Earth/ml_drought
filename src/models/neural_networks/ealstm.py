859a44de3c65a5bb0eb7f531537184572224214b
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-07-23 13:34:21 -0400

    Add EALSTM

---
+++ src/models/neural_networks/ealstm.py
@@ -0,0 +1,128 @@
+import torch
+from torch import nn
+
+
+class EALSTM(nn.Module):
+    """Implementation of the Entity-Aware-LSTM (EA-LSTM)
+
+    This code was copied from
+    https://github.com/kratzert/ealstm_regional_modeling/blob/master/papercode/ealstm.py
+
+    Parameters
+    ----------
+    input_size_dyn : int
+        Number of dynamic features, which are those, passed to the LSTM at each time step.
+    input_size_stat : int
+        Number of static features, which are those that are used to modulate the input gate.
+    hidden_size : int
+        Number of hidden/memory cells.
+    batch_first : bool, optional
+        If True, expects the batch inputs to be of shape [batch, seq, features] otherwise, the
+        shape has to be [seq, batch, features], by default True.
+    initial_forget_bias : int, optional
+        Value of the initial forget gate bias, by default 0
+    """
+
+    def __init__(self,
+                 input_size_dyn: int,
+                 input_size_stat: int,
+                 hidden_size: int,
+                 batch_first: bool = True,
+                 initial_forget_bias: int = 0):
+        super().__init__()
+
+        self.input_size_dyn = input_size_dyn
+        self.input_size_stat = input_size_stat
+        self.hidden_size = hidden_size
+        self.batch_first = batch_first
+        self.initial_forget_bias = initial_forget_bias
+
+        # create tensors of learnable parameters
+        self.weight_ih = nn.Parameter(torch.FloatTensor(input_size_dyn,  # type: ignore
+                                                        3 * hidden_size))  # type: ignore
+        self.weight_hh = nn.Parameter(torch.FloatTensor(hidden_size,  # type: ignore
+                                                        3 * hidden_size))  # type: ignore
+        self.weight_sh = nn.Parameter(torch.FloatTensor(input_size_stat,  # type: ignore
+                                                        hidden_size))  # type: ignore
+        self.bias = nn.Parameter(torch.FloatTensor(3 * hidden_size))  # type: ignore
+        self.bias_s = nn.Parameter(torch.FloatTensor(hidden_size))  # type: ignore
+
+        # initialize parameters
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        """Initialize all learnable parameters of the LSTM"""
+        nn.init.orthogonal_(self.weight_ih.data)
+        nn.init.orthogonal_(self.weight_sh)
+
+        weight_hh_data = torch.eye(self.hidden_size)
+        weight_hh_data = weight_hh_data.repeat(1, 3)
+        self.weight_hh.data = weight_hh_data
+
+        nn.init.constant_(self.bias.data, val=0)
+        nn.init.constant_(self.bias_s.data, val=0)
+
+        if self.initial_forget_bias != 0:
+            self.bias.data[:self.hidden_size] = self.initial_forget_bias
+
+    def forward(self, x_d, x_s):
+        """[summary]
+        Parameters
+        ----------
+        x_d : torch.Tensor
+            Tensor, containing a batch of sequences of the dynamic features. Shape has to match
+            the format specified with batch_first.
+        x_s : torch.Tensor
+            Tensor, containing a batch of static features.
+        Returns
+        -------
+        h_n : torch.Tensor
+            The hidden states of each time step of each sample in the batch.
+        c_n : torch.Tensor]
+            The cell states of each time step of each sample in the batch.
+        """
+        if self.batch_first:
+            x_d = x_d.transpose(0, 1)
+
+        seq_len, batch_size, _ = x_d.size()
+
+        h_0 = x_d.data.new(batch_size, self.hidden_size).zero_()
+        c_0 = x_d.data.new(batch_size, self.hidden_size).zero_()
+        h_x = (h_0, c_0)
+
+        # empty lists to temporally store all intermediate hidden/cell states
+        h_n, c_n = [], []
+
+        # expand bias vectors to batch size
+        bias_batch = (self.bias.unsqueeze(0).expand(batch_size, *self.bias.size()))
+
+        # calculate input gate only once because inputs are static
+        bias_s_batch = (self.bias_s.unsqueeze(0).expand(batch_size, *self.bias_s.size()))
+        i = torch.sigmoid(torch.addmm(bias_s_batch, x_s, self.weight_sh))
+
+        # perform forward steps over input sequence
+        for t in range(seq_len):
+            h_0, c_0 = h_x
+
+            # calculate gates
+            gates = (torch.addmm(bias_batch, h_0, self.weight_hh) +
+                     torch.mm(x_d[t], self.weight_ih))
+            f, o, g = gates.chunk(3, 1)
+
+            c_1 = torch.sigmoid(f) * c_0 + i * torch.tanh(g)
+            h_1 = torch.sigmoid(o) * torch.tanh(c_1)
+
+            # store intermediate hidden/cell state in list
+            h_n.append(h_1)
+            c_n.append(c_1)
+
+            h_x = (h_1, c_1)
+
+        h_n = torch.stack(h_n, 0)
+        c_n = torch.stack(c_n, 0)
+
+        if self.batch_first:
+            h_n = h_n.transpose(0, 1)
+            c_n = c_n.transpose(0, 1)
+
+        return h_n, c_n
