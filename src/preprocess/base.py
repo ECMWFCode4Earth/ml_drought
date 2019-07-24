e408452a804840eff2d0fb06818bff7ed969a1a0
tommylees112 <thomas.lees112@gmail.com>
2019-07-24 08:29:23 +0100

    fix mypy errors

--- src/preprocess/base.py
+++ src/preprocess/base.py
@@ -56,10 +56,10 @@ class BasePreProcessor:
             target_folder = self.interim
 
         if years is not None:
-            assert int(list(target_folder.glob('**'))[0])
+            assert int(list(target_folder.glob('**'))[0])  # type: ignore
             outfiles = [
                 f for f in target_folder.glob('**/*.nc')
-                if int(f.parents[0]) in years
+                if int(f.parents[0]) in years  # type: ignore
             ]
         else:
             outfiles = [
