44d05cdf554846508ac340bba7dfaa004e5f32f8
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-07-30 10:00:07 -0400

    Add tiff to netcdf converter

--- src/exporters/srtm.py
+++ src/exporters/srtm.py
@@ -1,4 +1,6 @@
 import elevation
+from osgeo import gdal
+from pathlib import Path
 
 from typing import Tuple
 
@@ -17,6 +19,12 @@ class SRTMExporter(BaseExporter):
     @staticmethod
     def _region_to_tuple(region: Region) -> Tuple[float, float, float, float]:
         return region.lonmin, region.latmin, region.lonmax, region.latmax
+
+    @staticmethod
+    def _tiff_to_nc(tif_file: Path, nc_file: Path) -> None:
+        ds = gdal.Open(tif_file.resolve().as_posix())
+        _ = gdal.Translate(format='NetCDF', srcDS=ds,
+                           destName=nc_file.resolve().as_posix())
 
     def export(self, region_name: str = 'kenya',
                product: str = 'SRTM3',
@@ -39,14 +47,22 @@ class SRTMExporter(BaseExporter):
 
         region = region_lookup[region_name]
 
-        output_name = self.output_folder / f'{region_name}.tif'
+        output_tif = self.output_folder / f'{region_name}.tif'
 
-        try:
-            elevation.clip(bounds=self._region_to_tuple(region),
-                           output=output_name.resolve().as_posix(),
-                           product=product,
-                           max_download_tiles=max_download_tiles)
-        except Exception as e:
-            print(e)
+        if not output_tif.exists():
+            print(f'Downloading tiles. Saving as tif to {output_tif}')
+            try:
+                elevation.clip(bounds=self._region_to_tuple(region),
+                               output=output_tif.resolve().as_posix(),
+                               product=product,
+                               max_download_tiles=max_download_tiles)
+            except Exception as e:
+                print(e)
 
-        elevation.clean()
+            elevation.clean()
+
+        output_nc = self.output_folder / f'{region_name}.nc'
+
+        if not output_nc.exists():
+            print(f'Converting {output_tif} to NetCDF format ({output_nc})')
+            self._tiff_to_nc(output_tif, output_nc)
