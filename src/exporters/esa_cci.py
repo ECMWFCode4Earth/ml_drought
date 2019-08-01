96bb348d5758967768778e90203ba6d2ad61c957
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-08-01 17:12:59 -0400

    Make sure files are unzipped to the right directory

--- src/exporters/esa_cci.py
+++ src/exporters/esa_cci.py
@@ -64,7 +64,7 @@ class ESACCIExporter(BaseExporter):
         assert fname.exists()
         print(f'Unzipping {fname.name}')
 
-        os.system(f'unzip {fname.as_posix()}')
+        os.system(f'unzip {fname.as_posix()} -d {self.landcover_folder.resolve().as_posix()}')
         print(f'{fname.name} unzipped!')
 
     def export(self) -> None:
