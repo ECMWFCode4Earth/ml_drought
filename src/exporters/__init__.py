a9954fc98b6efb858fc6e8b4fbe1094afabbc6fa
tommylees112 <thomas.lees112@gmail.com>
2019-08-05 16:49:33 +0100

    add other export functionality

--- src/exporters/__init__.py
+++ src/exporters/__init__.py
@@ -6,8 +6,9 @@ from .seas5.s5 import S5Exporter
 from .gleam import GLEAMExporter
 from .srtm import SRTMExporter
 from .esa_cci import ESACCIExporter
+from .admin_boundaries import KenyaAdmin
 
 __all__ = [
     'ERA5Exporter', 'VHIExporter', 'ERA5ExporterPOS',
     'CHIRPSExporter', 'S5Exporter', 'GLEAMExporter',
-    'SRTMExporter', 'ESACCIExporter']
+    'SRTMExporter', 'ESACCIExporter', 'KenyaAdmin']
