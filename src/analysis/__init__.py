9e6e1b262c75c85440c8601d8930179a64b5f71a
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-08-08 09:49:49 -0400

    Move evaluation functions into src

--- src/analysis/__init__.py
+++ src/analysis/__init__.py
@@ -1,5 +1,6 @@
 from .event_detector import EventDetector
 from .plot_shap import plot_shap_values
+from .evaluation import monthly_r2_score, annual_r2_scores, plot_predictions
 from .indices import (
     SPI,
     ZScoreIndex,
@@ -14,4 +15,5 @@ __all__ = [
     'plot_shap_values', 'EventDetector', 'SPI', 'ZScoreIndex',
     'PercentNormalIndex', 'DroughtSeverityIndex',
     'ChinaZIndex', 'DecileIndex', 'AnomalyIndex',
+    'monthly_r2_score', 'annual_r2_scores', 'plot_predictions'
 ]
