1d836b8a3d2e10f9ec1198147fbb3d0596660d0e
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-08-08 09:54:53 -0400

    Add explanations to all neural nets in the models script

--- scripts/models.py
+++ scripts/models.py
@@ -96,34 +96,7 @@ def linear_nn(
     predictor.evaluate(save_preds=True)
     predictor.save_model()
 
-    # The code below is commented out because of a bug in the shap deep Explainer which
-    # prevents it from working. It has been fixed in master, but not deployed yet:
-    # https://github.com/slundberg/shap/pull/684
-
-    # test_arrays_loader = DataLoader(data_path=data_path, batch_file_size=1,
-    #                                 shuffle_data=False, mode='test', to_tensor=True)
-    # key, val = list(next(iter(test_arrays_loader)).items())[0]
-    #
-    # explain_hist, explain_add = predictor.explain([val.x.historical[:3], val.x.pred_months[:3]])
-    # print(explain_hist.shape)
-    # np.save('shap_linear_network_hist.npy', explain_hist)
-    # np.save('shap_linear_network_add.npy', explain_add)
-    #
-    # np.save('shap_x_linear_network_hist.npy', val.x.historical[:3])
-    # np.save('shap_x_linear_network_add.npy', val.x.pred_months[:3])
-    #
-    # with open('variables_linear_network.txt', 'w') as f:
-    #     f.write(str(val.x_vars))
-    #
-    # # plot the variables
-    # with (data_path / 'features/normalizing_dict.pkl').open('rb') as f:
-    #     normalizing_dict = pickle.load(f)
-    #
-    # for variable in val.x_vars:
-    #     plt.clf()
-    #     plot_shap_values(val.x[0].numpy(), explain_hist[0], val.x_vars, normalizing_dict, variable,
-    #                      normalize_shap_plots=True, show=False)
-    #     plt.savefig(f'{variable}_linear_network.png', dpi=300, bbox_inches='tight')
+    _ = predictor.explain(save_shap_values=True)
 
 
 def rnn(
@@ -148,7 +121,7 @@ def rnn(
     predictor.evaluate(save_preds=True)
     predictor.save_model()
 
-    # See above; we need to update the shap version before this can be explained
+    _ = predictor.explain(save_shap_values=True)
 
 
 def earnn(
