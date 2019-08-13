39a587d9734955a28bcd057715b247e6c45f4caa
tommylees112 <thomas.lees112@gmail.com>
2019-08-13 19:46:35 +0100

    update scripts

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -2,19 +2,53 @@ from pathlib import Path
 import sys
 sys.path.append('..')
 
-from src.analysis import RegionAnalysis
+from src.analysis import AdministrativeRegionAnalysis
+from src.analysis import LandcoverRegionAnalysis
 
 
-def run_region_analysis():
+def run_administrative_region_analysis():
     # if the working directory is alread ml_drought don't need ../data
     if Path('.').absolute().as_posix().split('/')[-1] == 'ml_drought':
         data_path = Path('data')
     else:
         data_path = Path('../data')
 
-    analyzer = RegionAnalysis(data_dir)
+    assert [f for f in (data_path / 'features').glob('*/test/*/*.nc')] != [], \
+        'There is no true data (has the pipeline been run?)'
+    assert [f for f in (data_path / 'models').glob('*/*/*.nc')] != [], \
+        'There is no model data (has the pipeline been run?)'
+    assert [f for f in (data_path / 'analysis').glob('*/*.nc')] != [], \
+        'There are no processed regions. '\
+        'Has the pipeline been run?'
+
+    analyzer = AdministrativeRegionAnalysis(data_dir)
+    analyzer.analyze()
+
+
+def run_landcover_region_analysis():
+    # if the working directory is alread ml_drought don't need ../data
+    if Path('.').absolute().as_posix().split('/')[-1] == 'ml_drought':
+        data_path = Path('data')
+    else:
+        data_path = Path('../data')
+
+    assert [f for f in (data_path / 'features').glob('*/test/*/*.nc')] != [], \
+        'There is no true data (has the pipeline been run?)'
+    assert [f for f in (data_path / 'models').glob('*/*/*.nc')] != [], \
+        'There is no model data (has the pipeline been run?)'
+    assert [
+        f.name for f in (
+            data_path / 'interim'/ 'static' /
+            'esa_cci_landcover_preprocessed'
+        ).glob('*')
+    ] != [], \
+        'There is no landcover data. '\
+        'Has the pipeline been run?'
+
+    analyzer = LandcoverRegionAnalysis(data_dir)
     analyzer.analyze()
 
 
 if __name__ == '__main__':
-    run_region_analysis()
+    run_administrative_region_analysis()
+    run_landcover_region_analysis()
