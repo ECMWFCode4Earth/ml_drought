912065ab2bf8deab4159d72db30e7687b2e77064
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-08-13 14:43:46 -0400

    Fix legend location, allow for polished plot names

--- src/analysis/plot_shap.py
+++ src/analysis/plot_shap.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 
 def plot_shap_values(x: np.ndarray,
@@ -13,7 +13,8 @@ def plot_shap_values(x: np.ndarray,
                      normalizing_dict: Dict[str, Dict[str, float]],
                      value_to_plot: str,
                      normalize_shap_plots: bool = True,
-                     show: bool = False) -> None:
+                     show: bool = False,
+                     polished_value_name: Optional[str] = None) -> None:
     """Plots the denormalized values against their shap values, so that
     variations in the input features to the model can be compared to their effect
     on the model. For example plots, see notebooks/08_gt_recurrent_model.ipynb.
@@ -35,6 +36,8 @@ def plot_shap_values(x: np.ndarray,
         variable plots (on an instance specific basis).
     show: bool = False
         If True, a plot of the variable `value_to_plot` against its shap values will be plotted.
+    polished_value_name: Optional[str] = None
+        If passed to the model, this is used instead of value_to_plot when labelling the axes.
     """
     # first, lets isolate the lists
     idx = val_list.index(value_to_plot)
@@ -61,17 +64,20 @@ def plot_shap_values(x: np.ndarray,
     if normalize_shap_plots:
         par1.set_ylim(shap_values.min(), shap_values.max())
 
+    if polished_value_name is None:
+        polished_value_name = value_to_plot
+
     host.set_xlabel("Months")
-    host.set_ylabel(value_to_plot)
+    host.set_ylabel(polished_value_name)
     par1.set_ylabel("Shap value")
 
-    p1, = host.plot(months, x_val, label=value_to_plot)
+    p1, = host.plot(months, x_val, label=polished_value_name)
     p2, = par1.plot(months, shap_val, label="shap value")
 
     host.axis["left"].label.set_color(p1.get_color())
     par1.axis["right"].label.set_color(p2.get_color())
 
-    host.legend()
+    host.legend(loc=2)
 
     plt.draw()
     if show:
