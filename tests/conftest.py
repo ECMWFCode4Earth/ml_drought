67fee7fb8aabd6338fc986b9e4ac6b2980de8cad
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-09-03 08:19:49 +0700

    Ignore preprocessors without xesmf

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,3 +9,12 @@ try:
     import paramiko
 except ImportError:
     collect_ignore.append('exporters/test_gleam.py')
+
+try:
+    import xesmf
+except ImportError:
+    # pretty much all the preprocessors rely on
+    # xesmf for regridding. They'll all fail without it
+    # so this is an easier solutuon then adding an xfail
+    # to all the modules
+    collect_ignore.append('preprocess')
