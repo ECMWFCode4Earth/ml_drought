092b75e915921576dd44a2b9c4f1796012159ff3
tommylees112 <thomas.lees112@gmail.com>
2019-10-22 15:20:28 +0100

    remove the valid_time func

--- src/preprocess/seas5/s5.py
+++ src/preprocess/seas5/s5.py
@@ -161,25 +161,6 @@ class S5Preprocessor(BasePreProcessor):
             )
         return ds
 
-    def create_valid_time_ds(self, ds: xr.Dataset) -> xr.Dataset:
-        """"""
-        # calculate the valid times from the FH/ID
-        forecast_horizon = ds.forecast_horizon.values
-        initialisation_date = ds.initialisation_date.values
-        valid_time = initialisation_date[:, np.newaxis] + forecast_horizon
-
-        # create new dimension in the dataset object
-        stacked = ds.stack(
-            time=('initialisation_date', 'forecast_horizon')
-        )
-        stacked['time'] = (['initialisation_date', 'forecast_horizon'], valid_time)
-        stacked['forecast_horizon'] = forecast_horizon
-        stacked['initialisation_date'] = initialisation_date
-        ds['_time'] = (['initialisation_date', 'forecast_horizon'], valid_time)
-        ds = ds.assign_coords(valid_time=ds._time)
-        ds = ds.drop('_time')
-
-        return ds
 
     def preprocess(self, variable: str,
                    regrid: Optional[Path] = None,
