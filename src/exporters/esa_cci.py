0d264b084749498b0abfdc986b14d2ecb3cae7c1
tommylees112 <thomas.lees112@gmail.com>
2019-11-27 15:43:45 +0000

    update esa cci preprocess

--- src/exporters/esa_cci.py
+++ src/exporters/esa_cci.py
@@ -60,14 +60,19 @@ class ESACCIExporter(BaseExporter):
             return None
         os.system(f"wget {url_path}.zip -P {self.landcover_folder.as_posix()}")
 
-    def unzip(self) -> None:
+    def unzip(self, python_only: bool = True) -> None:
+        """https://stackoverflow.com/a/3451150/9940782
+        """
         fname = self.landcover_folder / (self.target_file + ".zip")
         assert fname.exists()
         print(f"Unzipping {fname.name}")
-
-        os.system(
-            f"unzip {fname.as_posix()} -d {self.landcover_folder.resolve().as_posix()}"
-        )
+        if python_only:
+            with zipfile.ZipFile(fname, 'r') as zip_ref:
+                zip_ref.extractall(fname.parents[0])
+        else:  # ECMWF machine doesn't have the bash `unzip` utility
+            os.system(
+                f"unzip {fname.as_posix()} -d {self.landcover_folder.resolve().as_posix()}"
+            )
         print(f"{fname.name} unzipped!")
 
     def export(self) -> None:
