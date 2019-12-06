6de52afe508609799dba4fcdbcd025ad9a270516
tommylees112 <thomas.lees112@gmail.com>
2019-12-06 10:43:20 +0000

    update get_data_path in scripts/utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -5,6 +5,8 @@ def get_data_path() -> Path:
     # if the working directory is alread ml_drought don't need ../data
     if Path(".").absolute().as_posix().split("/")[-1] == "ml_drought":
         data_path = Path("data")
+    elif Path(".").absolute().as_posix().split("/")[-3] == "ml_drought":
+        data_path = Path("../../../data")
     else:
         data_path = Path("../data")
     return data_path
