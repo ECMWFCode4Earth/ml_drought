d5e216bdf8908785c07c27b33ffe0ee8478bba70
tommylees112 <thomas.lees112@gmail.com>
2019-12-17 12:09:51 +0000

    update models for different variables experiments

--- scripts/experiments/00_models.py
+++ scripts/experiments/00_models.py
@@ -2,164 +2,21 @@ import sys
 
 sys.path.append("../..")
 
-from scripts.utils import get_data_path, _rename_directory
-from src.analysis import all_shap_for_file
-from src.models import (
-    Persistence,
-    LinearRegression,
-    LinearNetwork,
-    RecurrentNetwork,
-    EARecurrentNetwork,
-    load_model,
-    GBDT,
+from ._base_models import (
+    parsimonious,
+    regression,
+    linear_nn,
+    rnn,
+    earnn,
 )
 
+# if __name__ == "__main__":
+#     always_ignore_vars = ["ndvi", "p84.162", "sp", "tp", "Eb", "E", "p0001"]
+#     important_vars = ["VCI", "precip", "t2m", "pev", "p0005", "SMsurf", "SMroot"]
 
-def parsimonious(experiment="one_month_forecast",):
-    predictor = Persistence(get_data_path(), experiment=experiment)
-    predictor.evaluate(save_preds=True)
-
-
-def regression(
-    experiment="one_month_forecast",
-    include_pred_month=True,
-    surrounding_pixels=1,
-    explain=False,
-    static="features",
-    ignore_vars=None,
-):
-    predictor = LinearRegression(
-        get_data_path(),
-        experiment=experiment,
-        include_pred_month=include_pred_month,
-        surrounding_pixels=surrounding_pixels,
-        static=static,
-        ignore_vars=ignore_vars,
-    )
-    predictor.train()
-    predictor.evaluate(save_preds=True)
-
-    # mostly to test it works
-    if explain:
-        predictor.explain(save_shap_values=True)
-
-
-def linear_nn(
-    experiment="one_month_forecast",
-    include_pred_month=True,
-    surrounding_pixels=1,
-    explain=False,
-    static="features",
-    ignore_vars=None,
-):
-    predictor = LinearNetwork(
-        layer_sizes=[100],
-        data_folder=get_data_path(),
-        experiment=experiment,
-        include_pred_month=include_pred_month,
-        surrounding_pixels=surrounding_pixels,
-        static=static,
-        ignore_vars=ignore_vars,
-    )
-    predictor.train(num_epochs=50, early_stopping=5)
-    predictor.evaluate(save_preds=True)
-    predictor.save_model()
-
-    if explain:
-        _ = predictor.explain(save_shap_values=True)
-
-
-def rnn(
-    experiment="one_month_forecast",
-    include_pred_month=True,
-    surrounding_pixels=1,
-    explain=False,
-    static="features",
-    ignore_vars=None,
-):
-    predictor = RecurrentNetwork(
-        hidden_size=128,
-        data_folder=get_data_path(),
-        experiment=experiment,
-        include_pred_month=include_pred_month,
-        surrounding_pixels=surrounding_pixels,
-        static=static,
-        ignore_vars=ignore_vars,
-    )
-    predictor.train(num_epochs=50, early_stopping=5)
-    predictor.evaluate(save_preds=True)
-    predictor.save_model()
-
-    if explain:
-        _ = predictor.explain(save_shap_values=True)
-
-
-def earnn(
-    experiment="one_month_forecast",
-    include_pred_month=True,
-    surrounding_pixels=None,
-    pretrained=False,
-    explain=False,
-    static="features",
-    ignore_vars=None,
-):
-    data_path = get_data_path()
-
-    if not pretrained:
-        predictor = EARecurrentNetwork(
-            hidden_size=248,
-            data_folder=data_path,
-            experiment=experiment,
-            include_pred_month=include_pred_month,
-            surrounding_pixels=surrounding_pixels,
-            static=static,
-            static_embedding_size=200,
-            ignore_vars=ignore_vars,
-        )
-        predictor.train(num_epochs=50, early_stopping=5)
-        predictor.evaluate(save_preds=True)
-        predictor.save_model()
-    else:
-        predictor = load_model(data_path / f"models/{experiment}/ealstm/model.pt")
-
-    if explain:
-        test_file = data_path / f"features/{experiment}/test/2018_3"
-        assert test_file.exists()
-        all_shap_for_file(test_file, predictor, batch_size=100)
-
-
-def gbdt(
-    experiment="one_month_forecast",
-    include_pred_month=True,
-    surrounding_pixels=None,
-    pretrained=True,
-    explain=False,
-    static="features",
-    ignore_vars=None,
-):
-    data_path = get_data_path()
-
-    # initialise, train and save GBDT model
-    predictor = GBDT(
-        data_folder=data_path,
-        experiment=experiment,
-        include_pred_month=include_pred_month,
-        surrounding_pixels=surrounding_pixels,
-        static=static,
-        ignore_vars=ignore_vars,
-    )
-    predictor.train(early_stopping=5)
-    predictor.evaluate(save_preds=True)
-    predictor.save_model()
-
-
-if __name__ == "__main__":
-    always_ignore_vars = ["ndvi", "p84.162", "sp", "tp", "Eb", "E", "p0001"]
-    important_vars = ["VCI", "precip", "t2m", "pev", "p0005", "SMsurf", "SMroot"]
-
-    parsimonious()
-    regression(ignore_vars=always_ignore_vars)
-    linear_nn(ignore_vars=always_ignore_vars)
-    rnn(ignore_vars=always_ignore_vars)
-    earnn(ignore_vars=always_ignore_vars)
-    gbdt(ignore_vars=always_ignore_vars)
+#     parsimonious()
+#     regression(ignore_vars=always_ignore_vars)
+#     linear_nn(ignore_vars=always_ignore_vars)
+#     rnn(ignore_vars=always_ignore_vars)
+#     earnn(ignore_vars=always_ignore_vars)
+#     gbdt(ignore_vars=always_ignore_vars)
---
+++ scripts/experiments/_base_models.py
@@ -0,0 +1,164 @@
+from src.models import (
+    Persistence,
+    LinearRegression,
+    LinearNetwork,
+    RecurrentNetwork,
+    EARecurrentNetwork,
+    load_model,
+    GBDT,
+)
+from src.analysis import all_shap_for_file
+from scripts.utils import get_data_path
+import sys
+
+sys.path.append("../..")
+
+
+def parsimonious(experiment="one_month_forecast",):
+    predictor = Persistence(get_data_path(), experiment=experiment)
+    predictor.evaluate(save_preds=True)
+
+
+def regression(
+    experiment="one_month_forecast",
+    include_pred_month=True,
+    surrounding_pixels=None,
+    explain=False,
+    static="features",
+    ignore_vars=None,
+):
+    predictor = LinearRegression(
+        get_data_path(),
+        experiment=experiment,
+        include_pred_month=include_pred_month,
+        surrounding_pixels=surrounding_pixels,
+        static=static,
+        ignore_vars=ignore_vars,
+    )
+    predictor.train()
+    predictor.evaluate(save_preds=True)
+
+    # mostly to test it works
+    if explain:
+        predictor.explain(save_shap_values=True)
+
+
+def linear_nn(
+    experiment="one_month_forecast",
+    include_pred_month=True,
+    surrounding_pixels=None,
+    explain=False,
+    static="features",
+    ignore_vars=None,
+    num_epochs=50,
+    early_stopping=5,
+    layer_sizes=[100],
+):
+    predictor = LinearNetwork(
+        layer_sizes=layer_sizes,
+        data_folder=get_data_path(),
+        experiment=experiment,
+        include_pred_month=include_pred_month,
+        surrounding_pixels=surrounding_pixels,
+        static=static,
+        ignore_vars=ignore_vars
+    )
+    predictor.train(num_epochs=num_epochs, early_stopping=early_stopping)
+    predictor.evaluate(save_preds=True)
+    predictor.save_model()
+
+    if explain:
+        _ = predictor.explain(save_shap_values=True)
+
+
+def rnn(
+    experiment="one_month_forecast",
+    include_pred_month=True,
+    surrounding_pixels=None,
+    explain=False,
+    static="features",
+    ignore_vars=None,
+    num_epochs=50,
+    early_stopping=5,
+    hidden_size=128,
+):
+    predictor = RecurrentNetwork(
+        hidden_size=hidden_size,
+        data_folder=get_data_path(),
+        experiment=experiment,
+        include_pred_month=include_pred_month,
+        surrounding_pixels=surrounding_pixels,
+        static=static,
+        ignore_vars=ignore_vars
+    )
+    predictor.train(num_epochs=num_epochs, early_stopping=early_stopping)
+    predictor.evaluate(save_preds=True)
+    predictor.save_model()
+
+    if explain:
+        _ = predictor.explain(save_shap_values=True)
+
+
+def earnn(
+    experiment="one_month_forecast",
+    include_pred_month=True,
+    surrounding_pixels=None,
+    pretrained=False,
+    explain=False,
+    static="features",
+    ignore_vars=None,
+    num_epochs=50,
+    early_stopping=5,
+    static_embedding_size=10,
+    hidden_size=128,
+):
+    data_path = get_data_path()
+
+    if not pretrained:
+        predictor = EARecurrentNetwork(
+            hidden_size=hidden_size,
+            data_folder=data_path,
+            experiment=experiment,
+            include_pred_month=include_pred_month,
+            surrounding_pixels=surrounding_pixels,
+            static=static,
+            static_embedding_size=static_embedding_size,
+            ignore_vars=ignore_vars
+        )
+        predictor.train(num_epochs=num_epochs, early_stopping=early_stopping)
+        predictor.evaluate(save_preds=True)
+        predictor.save_model()
+    else:
+        predictor = load_model(
+            data_path / f"models/{experiment}/ealstm/model.pt")
+
+    if explain:
+        test_file = data_path / f"features/{experiment}/test/2018_3"
+        assert test_file.exists()
+        all_shap_for_file(test_file, predictor, batch_size=100)
+
+
+def gbdt(
+    experiment="one_month_forecast",
+    include_pred_month=True,
+    surrounding_pixels=None,
+    pretrained=True,
+    explain=False,
+    static="features",
+    ignore_vars=None
+):
+    data_path = get_data_path()
+
+    # initialise, train and save GBDT model
+    predictor = GBDT(
+        data_folder=data_path,
+        experiment=experiment,
+        include_pred_month=include_pred_month,
+        surrounding_pixels=surrounding_pixels,
+        static=static,
+        ignore_vars=ignore_vars
+    )
+    predictor.train(early_stopping=5)
+    predictor.evaluate(save_preds=True)
+    predictor.save_model()
+
