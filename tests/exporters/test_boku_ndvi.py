f98877506109d13356d5b8e355f03dd94d4e5600
tommylees112 <thomas.lees112@gmail.com>
2020-01-20 10:53:07 +0000

    create initial tests

---
+++ tests/exporters/test_boku_ndvi.py
@@ -0,0 +1,134 @@
+import urllib.request
+from unittest.mock import patch, MagicMock
+import os
+from pathlib import Path
+
+from src.exporters.boku_ndvi import BokuNDVIExporter
+
+
+class TestBokuNDVIExporter:
+
+    africa_url = (
+        "ftp://ftp.chg.ucsb.edu/pub/org/chg/products/CHIRPS-2.0/africa_pentad/tifs/"
+    )
+    global_url = (
+        "ftp://ftp.chg.ucsb.edu/pub/org/chg/products/CHIRPS-2.0/global_pentad/netcdf/"
+    )
+
+    @staticmethod
+    def build_dummy_ftp_(tmp_path):
+        conda_prefix = Path(os.environ.get("CONDA_PREFIX"))
+        # make the associated directories and files
+        (conda_prefix / 'etc/conda/activate.d/').mkdir(parents=True, exist_okay=True)
+        (conda_prefix / 'etc/conda/deactivate.d/').mkdir(parents=True, exist_okay=True)
+        (conda_prefix / 'etc/conda/activate.d/env_vars.sh').touch()
+        (conda_prefix / 'etc/conda/deactivate.d/env_vars.sh').touch()
+
+        write_to_file = """
+        #!/bin/sh
+
+        export FTP_1000='ftp://1000'
+        export FTP_250='ftp://250'
+        """
+        with open(
+            conda_prefix / 'etc/conda/activate.d/env_vars.sh', 'wb'
+        ) as fp:
+            fp.write(write_to_file)
+
+        write_to_file = """
+        #!/bin/sh
+
+        unset FTP_1000
+        unset FTP_250
+        """
+        with open(
+            conda_prefix / 'etc/conda/deactivate.d/env_vars.sh', 'wb'
+        ) as fp:
+            fp.write(write_to_file)
+
+    def test_init(self, tmp_path):
+        BokuNDVIExporter(tmp_path)
+        assert (
+            tmp_path / "raw/boku_ndvi"
+        ).exists(), "Expected a raw/boku_ndvi folder to be created!"
+
+    @patch("os.system", autospec=True)
+    def test_checkpointing(self, mock_system, tmp_path, capsys):
+        # checks we don't redownload files
+
+        exporter = BokuNDVIExporter(tmp_path)
+        exporter.region_folder = exporter.output_folder / "global"
+        exporter.region_folder.mkdir()
+
+        # setup the already downloaded file
+        test_filename = "testy_test.nc"
+        (tmp_path / f"raw/boku_ndvi/global/{test_filename}").touch()
+
+        exporter.wget_file(test_filename)
+        captured = capsys.readouterr()
+
+        expected_stdout = f"{test_filename} already exists! Skipping\n"
+        assert (
+            captured.out == expected_stdout
+        ), f"Expected stdout to be {expected_stdout}, got {captured.out}"
+        mock_system.assert_not_called(), "os.system was called! Should have been skipped"
+
+    @patch("urllib.request.Request", autospec=True)
+    def test_get_filenames(self, request_patch, monkeypatch, tmp_path):
+        # First 1000 characters of the urllib response from the ftp for africa data,
+        # pulled on May 23 2019
+        request_patch.return_value = MagicMock()
+        response = (
+            "-rw-r--r--    1 31094    31111     4172329 Feb 01  2015 "
+            "chirps-v2.0.1981.01.1.tif.gz\r\n-rw-r--r--    1 31094    "
+            "31111     4133059 Feb 01  2015 chirps-v2.0.1981.01.2.tif."
+            "gz\r\n-rw-r--r--    1 31094    31111     3948001 Feb 01  20"
+            "15 chirps-v2.0.1981.01.3.tif.gz\r\n-rw-r--r--    1 31094    "
+            "31111     3942431 Feb 01  2015 chirps-v2.0.1981.01.4.tif.g"
+            "z\r\n-rw-r--r--    1 31094    31111     4176578 Feb 01  201"
+            "5 chirps-v2.0.1981.01.5.tif.gz\r\n-rw-r--r--    1 31094    "
+            "31111     4333592 Feb 01  2015 chirps-v2.0.1981.01.6.tif."
+            "gz\r\n-rw-r--r--    1 31094    31111     4129989 Feb 02  2015"
+            " chirps-v2.0.1981.02.1.tif.gz\r\n-rw-r--r--    1 31094    3"
+            "1111     4067482 Feb 02  2015 chirps-v2.0.1981.02.2.tif."
+            "gz\r\n-rw-r--r--    1 31094    31111     3927390 Feb 02  2015"
+            " chirps-v2.0.1981.02.3.tif.gz\r\n-rw-r--r--    1 31094    311"
+            "11     4208427 Feb 02  2015 chirps-v2.0.1981.02.4.tif.gz\r\n-"
+            "rw-r--r--    1 31094    31111     4301096 Feb 02  2015 chirps-v"
+            "2.0.1981.02.5.tif.gz\r\n"
+        )
+
+        expected_filenames = [
+            "chirps-v2.0.1981.01.1.tif.gz",
+            "chirps-v2.0.1981.01.2.tif.gz",
+            "chirps-v2.0.1981.01.3.tif.gz",
+            "chirps-v2.0.1981.01.4.tif.gz",
+            "chirps-v2.0.1981.01.5.tif.gz",
+            "chirps-v2.0.1981.01.6.tif.gz",
+            "chirps-v2.0.1981.02.1.tif.gz",
+            "chirps-v2.0.1981.02.2.tif.gz",
+            "chirps-v2.0.1981.02.3.tif.gz",
+            "chirps-v2.0.1981.02.4.tif.gz",
+            "chirps-v2.0.1981.02.5.tif.gz",
+        ]
+
+        def mockreturn(request):
+            class OpenURL:
+                def read(self):
+                    return response
+
+            open_url = OpenURL()
+            return open_url
+
+        monkeypatch.setattr(urllib.request, "urlopen", mockreturn)
+
+        exporter = BokuNDVIExporter(tmp_path)
+        filenames = exporter.get_chirps_filenames(
+            region="africa", period="pentad")
+
+        assert filenames == expected_filenames
+
+        # Can't get assert_called_with to work, this is a hackey workaround
+        assert (
+            request_patch.call_args[0][0] == self.africa_url
+        ), f"Expected the urllib request to use following url: {self.africa_url}"
---
+++ tests/preprocess/test_boku_ndvi.py
@@ -0,0 +1,151 @@
+import xarray as xr
+import numpy as np
+from datetime import datetime
+
+from src.preprocess.boku_ndvi import BokuNDVIPreprocessor
+
+from src.utils import get_kenya, get_ethiopia
+
+from ..utils import _make_dataset
+
+
+class TestBokuNDVIPreprocessor:
+    @staticmethod
+    def test_make_filename():
+
+        test_file = "testy_test.nc"
+        expected_output = "testy_test_kenya.nc"
+
+        filename = BokuNDVIPreprocessor.create_filename(test_file, "kenya")
+        assert (
+            filename == expected_output
+        ), f"Expected output to be {expected_output}, got {filename}"
+
+    @staticmethod
+    def _make_chirps_dataset(
+        size, lonmin=-180.0, lonmax=180.0, latmin=-55.152, latmax=75.024, add_times=True
+    ):
+        lat_len, lon_len = size
+        # create the vector
+        longitudes = np.linspace(lonmin, lonmax, lon_len)
+        latitudes = np.linspace(latmin, latmax, lat_len)
+
+        dims = ["latitude", "longitude"]
+        coords = {"latitude": latitudes, "longitude": longitudes}
+
+        if add_times:
+            size = (2, size[0], size[1])
+            dims.insert(0, "time")
+            coords["time"] = [datetime(2019, 1, 1), datetime(2019, 1, 2)]
+        vhi = np.random.randint(100, size=size)
+
+        return xr.Dataset({"VHI": (dims, vhi)}, coords=coords)
+
+    @staticmethod
+    def test_directories_created(tmp_path):
+        v = BokuNDVIPreprocessor(tmp_path)
+
+        assert (
+            tmp_path / v.preprocessed_folder / "chirps_preprocessed"
+        ).exists(), (
+            "Should have created a directory tmp_path/interim/chirps_preprocessed"
+        )
+
+        assert (
+            tmp_path / v.preprocessed_folder / "chirps_interim"
+        ).exists(), "Should have created a directory tmp_path/interim/chirps_interim"
+
+    @staticmethod
+    def test_get_filenames(tmp_path):
+
+        (tmp_path / "raw" / "chirps").mkdir(parents=True)
+
+        test_file = tmp_path / "raw/chirps/testy_test.nc"
+        test_file.touch()
+
+        processor = BokuNDVIPreprocessor(tmp_path)
+
+        files = processor.get_filepaths()
+        assert files[0] == test_file, f"Expected {test_file} to be retrieved"
+
+    def test_preprocess(self, tmp_path):
+
+        (tmp_path / "raw/chirps/global").mkdir(parents=True)
+        data_path = tmp_path / "raw/chirps/global/testy_test.nc"
+        dataset = self._make_chirps_dataset(size=(100, 100))
+        dataset.to_netcdf(path=data_path)
+
+        kenya = get_kenya()
+        regrid_dataset, _, _ = _make_dataset(
+            size=(20, 20),
+            latmin=kenya.latmin,
+            latmax=kenya.latmax,
+            lonmin=kenya.lonmin,
+            lonmax=kenya.lonmax,
+        )
+
+        regrid_path = tmp_path / "regridder.nc"
+        regrid_dataset.to_netcdf(regrid_path)
+
+        processor = BokuNDVIPreprocessor(tmp_path)
+        processor.preprocess(subset_str="kenya",
+                             regrid=regrid_path, parallel=False)
+
+        expected_out_path = tmp_path / "interim/chirps_preprocessed/data_kenya.nc"
+        assert (
+            expected_out_path.exists()
+        ), f"Expected processed file to be saved to {expected_out_path}"
+
+        # check the subsetting happened correctly
+        out_data = xr.open_dataset(expected_out_path)
+        expected_dims = ["lat", "lon", "time"]
+        assert len(list(out_data.dims)) == len(expected_dims)
+        for dim in expected_dims:
+            assert dim in list(
+                out_data.dims
+            ), f"Expected {dim} to be in the processed dataset dims"
+
+        lons = out_data.lon.values
+        assert (lons.min() >= kenya.lonmin) and (
+            lons.max() <= kenya.lonmax
+        ), "Longitudes not correctly subset"
+
+        lats = out_data.lat.values
+        assert (lats.min() >= kenya.latmin) and (
+            lats.max() <= kenya.latmax
+        ), "Latitudes not correctly subset"
+
+        assert out_data.VHI.values.shape[1:] == (20, 20)
+
+        assert (
+            not processor.interim.exists()
+        ), f"Interim chirps folder should have been deleted"
+
+    def test_alternative_region(self, tmp_path):
+        # make the dataset
+        (tmp_path / "raw/chirps/global").mkdir(parents=True)
+        data_path = tmp_path / "raw/chirps/global/testy_test.nc"
+        dataset = self._make_chirps_dataset(size=(100, 100))
+        dataset.to_netcdf(path=data_path)
+        ethiopia = get_ethiopia()
+
+        # regrid the datasets
+        regrid_dataset, _, _ = _make_dataset(
+            size=(20, 20),
+            latmin=ethiopia.latmin,
+            latmax=ethiopia.latmax,
+            lonmin=ethiopia.lonmin,
+            lonmax=ethiopia.lonmax,
+        )
+        regrid_path = tmp_path / "regridder.nc"
+        regrid_dataset.to_netcdf(regrid_path)
+
+        # build the Preprocessor object and subset with a different subset_str
+        processor = BokuNDVIPreprocessor(tmp_path)
+        processor.preprocess(subset_str="ethiopia",
+                             regrid=regrid_path, parallel=False)
+
+        expected_out_path = tmp_path / "interim/chirps_preprocessed/data_ethiopia.nc"
+        assert (
+            expected_out_path.exists()
+        ), f"Expected processed file to be saved to {expected_out_path}"
