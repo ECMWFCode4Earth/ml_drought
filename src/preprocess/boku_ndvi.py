98b6c1697b19820a9eea19c7d55b631d80a8d111
tommylees112 <thomas.lees112@gmail.com>
2020-02-03 23:56:06 +0000

    remove the limit on the file indexes

--- src/preprocess/boku_ndvi.py
+++ src/preprocess/boku_ndvi.py
@@ -257,8 +257,6 @@ class BokuNDVIPreprocessor(BasePreProcessor):
             print("\nOutputs (errors):\n\t", outputs)
         else:
             for ix, file in enumerate(nc_files):
-                if ix < 556:
-                    continue
                 print(f"INDEX: {ix}")
                 self._preprocess_single(file, subset_str, regrid)
 
