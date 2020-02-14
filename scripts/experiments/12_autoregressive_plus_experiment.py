b96a696bc44c6372d73ba9c2be6c006a0e164528
tommylees112 <thomas.lees112@gmail.com>
2020-02-14 08:40:47 +0000

    update script experiment

--- scripts/experiments/12_autoregressive_plus_experiment.py
+++ scripts/experiments/12_autoregressive_plus_experiment.py
@@ -10,11 +10,16 @@ from scripts.utils import _rename_directory, get_data_path
 from src.engineer import Engineer
 
 
-def engineer(experiment="one_month_forecast", process_static=False, pred_months=3):
+def engineer(
+    experiment="one_month_forecast",
+    process_static=False,
+    pred_months=3,
+    target_var="boku_VCI",
+):
     engineer = Engineer(get_data_path(), experiment=experiment, process_static=False)
     engineer.engineer(
         test_year=[y for y in range(2016, 2019)],
-        target_variable="boku_VCI",
+        target_variable=target_var,
         pred_months=pred_months,
         expected_length=pred_months,
     )
@@ -27,7 +32,7 @@ def lstm(ignore_vars, static):
         explain=False,
         ignore_vars=ignore_vars,
         num_epochs=50,
-        early_stopping=5,
+        early_stopping=10,
         hidden_size=256,
         # static data
         static=static,
@@ -48,7 +53,7 @@ def ealstm(ignore_vars, static="features"):
         explain=False,
         ignore_vars=ignore_vars,
         num_epochs=50,
-        early_stopping=5,
+        early_stopping=10,
         hidden_size=256,
         static_embedding_size=64,
         # static data
@@ -59,8 +64,60 @@ def ealstm(ignore_vars, static="features"):
     )
 
 
+def rename_model_experiment_file(
+    data_dir: Path, vars_: List[str], static: bool, target_var: str
+) -> None:
+    vars_joined = "_".join(vars_)
+    from_path = data_dir / "models" / "one_month_forecast"
+    if static:
+        to_path = (
+            data_dir
+            / "models"
+            / f"one_month_forecast_{vars_joined}_YESstatic_{target_var}"
+        )
+    else:
+        to_path = (
+            data_dir
+            / "models"
+            / f"one_month_forecast_{vars_joined}_NOstatic_{target_var}"
+        )
+
+    # with_datetime ensures that unique
+    _rename_directory(from_path, to_path, with_datetime=True)
+
+
+def main(target_var, all_vars):
+    # RUN engineer
+    engineer(target_var=target_var)
+
+    autoregressive = [target_var]  # 'VCI3M'
+    dynamic = ["precip", "t2m", "pet", "E", "SMroot", "SMsurf"]
+    static_list = [False, False, True]
+
+    for vars_to_include, static_bool in zip(
+        [autoregressive, autoregressive + dynamic, autoregressive + dynamic],
+        static_list,
+    ):
+        print(
+            f'\n{"-" * 10}\nRunning experiment with: {vars_to_include} with static: {static_bool} for {target_var}\n{"-" * 10}'
+        )
+
+        # FIT models
+        vars_to_exclude = [v for v in all_vars if v not in vars_to_include]
+        if static_bool:
+            lstm(vars_to_exclude, static="features")
+            ealstm(vars_to_exclude, static="features")
+        else:
+            lstm(vars_to_exclude, static=None)
+
+        # RENAME model directories
+        data_dir = get_data_path()
+        rename_model_experiment_file(
+            data_dir, vars_to_include, static=static_bool, target_var=target_var
+        )
+
+
 if __name__ == "__main__":
-    engineer()
     # run the autoregressive LSTM
     # run the autoregressive + dynamic LSTM
     # run the autoregressive + dynamic + static LSTM / EALSTM
@@ -77,26 +134,13 @@ if __name__ == "__main__":
         "tp",
         "ndvi",
         "p84.162",
-        "boku_vci",
+        "boku_VCI",
         "VCI3M",
         "modis_ndvi",
     ]
 
-    autoregressive = ["boku_vci"]  # 'VCI3M'
-    dynamic = ["precip", "t2m", "pet", "E", "SMroot", "SMsurf"]
-    static_list = [False, False, True]
+    #  run the engineer
+    target_vars = ["boku_VCI", "VCI"]
 
-    for vars_to_include, static_bool in zip(
-        [autoregressive, autoregressive + dynamic, autoregressive + dynamic],
-        static_list,
-    ):
-        print(
-            f'\n{"-" * 10}\nRunning experiment with: {vars_to_include} with static: {static_bool}\n{"-" * 10}'
-        )
-
-        vars_to_exclude = [v for v in all_vars if v not in vars_to_include]
-        if static_bool:
-            lstm(vars_to_exclude, static="features")
-            ealstm(vars_to_exclude, static="features")
-        else:
-            lstm(vars_to_exclude, static=None)
+    for target_var in target_vars:
+        main(target_var, all_vars)
