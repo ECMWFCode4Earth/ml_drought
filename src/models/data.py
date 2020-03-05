db5be4403e37c86da666d8bfa6d31b5b1cfcda5f
tommylees112 <thomas.lees112@gmail.com>
2020-03-05 10:08:00 +0000

    why no data being loaded?

--- src/models/data.py
+++ src/models/data.py
@@ -821,7 +821,7 @@ class _BaseIter:
             target_time=target_time,
             historical_times=x_datetimes,
         )
-        assert False
+
         if to_tensor:
             model_arrays.to_tensor(self.device)
 
@@ -831,7 +831,7 @@ class _BaseIter:
             historical_target_np = self._calculate_historical_target(x, y_var)
             historical_target_np = historical_target_np[notnan_indices].flatten()
             model_arrays.historical_target = historical_target_np
-
+        assert False
         return model_arrays
 
     @staticmethod
