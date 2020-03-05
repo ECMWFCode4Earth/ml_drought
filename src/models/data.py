a9040f7b957bac7963c1ebef7b3f79005b9cf29a
tommylees112 <thomas.lees112@gmail.com>
2020-03-05 13:00:11 +0000

    why no data being loaded?

--- src/models/data.py
+++ src/models/data.py
@@ -832,7 +832,7 @@ class _BaseIter:
             historical_target_np = historical_target_np[notnan_indices].flatten()
             model_arrays.historical_target = historical_target_np
 
-        return model_arrays, (train_data, y_np)
+        return model_arrays  # , (train_data, y_np)
 
     @staticmethod
     def _add_extra_dims(
@@ -898,7 +898,7 @@ class _TrainIter(_BaseIter):
             cur_max_idx = min(self.idx + self.batch_file_size, self.max_idx)
             while self.idx < cur_max_idx:
                 subfolder = self.data_files[self.idx]
-                arrays, (train_data, y_np) = self.ds_folder_to_np(
+                arrays = self.ds_folder_to_np(  # , (train_data, y_np)
                     subfolder, clear_nans=self.clear_nans, to_tensor=False
                 )
                 if arrays.x.historical.shape[0] == 0:
@@ -950,7 +950,7 @@ class _TestIter(_BaseIter):
             cur_max_idx = min(self.idx + self.batch_file_size, self.max_idx)
             while self.idx < cur_max_idx:
                 subfolder = self.data_files[self.idx]
-                arrays, (train_data, y_np) = self.ds_folder_to_np(
+                arrays = self.ds_folder_to_np(
                     subfolder, clear_nans=self.clear_nans, to_tensor=self.to_tensor
                 )
                 if arrays.x.historical.shape[0] == 0:
