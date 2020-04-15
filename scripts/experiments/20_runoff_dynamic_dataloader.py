04e3395b5eced9d02ed066a7fba8786c902cc006
tommylees112 <thomas.lees112@gmail.com>
2020-04-15 13:24:44 +0100

    update models

--- scripts/experiments/20_runoff_dynamic_dataloader.py
+++ scripts/experiments/20_runoff_dynamic_dataloader.py
@@ -111,7 +111,7 @@ def main():
     target_var = "discharge_spec"
     seq_length = 365
     forecast_horizon = 1
-    logy = False
+    logy = True
     batch_size = 2000  # 1000
     catchment_ids = ["12002", "15006", "27009", "27034", "27041", "39001", "39081", "43021", "47001", "54001", "54057", "71001", "84013",]
     catchment_ids = [int(c_id) for c_id in catchment_ids]
@@ -160,5 +160,5 @@ def evaluate_only():
 
 
 if __name__ == "__main__":
-    # main()
-    evaluate_only()
+    main()
+    # evaluate_only()
