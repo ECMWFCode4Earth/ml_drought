4963d6c49213968672908ce3e168427b138f224a
tommylees112 <thomas.lees112@gmail.com>
2020-06-17 12:23:33 +0100

    install bash scrip

---
+++ scripts/experiments/INSTALL.sh
@@ -0,0 +1,6 @@
+conda install -c conda-forge xarray=0.15.1 --yes
+conda install -c conda-forge shap==0.30 --yes
+conda install h5py --yes
+pip install netcdf4
+pip install pytorch-lightning
+conda install -c conda-forge xesmf --yes
