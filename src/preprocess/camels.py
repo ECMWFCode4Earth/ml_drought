ca6255c876d16a689ae7d28fc783234c8d699497
tommylees112 <thomas.lees112@gmail.com>
2020-06-17 16:52:39 +0100

    update preprocess

--- src/preprocess/camels.py
+++ src/preprocess/camels.py
@@ -134,14 +134,13 @@ class CAMELSGBPreprocessor(BasePreProcessor):
 
         # join into one dataframe
         static_df = pd.concat(static_dfs, axis=1)
-        gauge_id = static_df['gauge_id'].iloc[:, 0]
-        static_df = static_df.drop('gauge_id', axis=1)
-        static_df['gauge_id'] = gauge_id
 
         # create xr object
         static_vars = [c for c in static_df.columns if c != "gauge_id"]
         dims = ["station_id"]
-        coords = {"station_id": static_df["gauge_id"].values}
+
+        gauge_id = static_df['gauge_id'].iloc[:, 0]
+        coords = {"station_id": gauge_id}
 
         static_ds = xr.Dataset(
             {
