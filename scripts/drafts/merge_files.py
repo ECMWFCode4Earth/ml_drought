81f43e02c4f2b94e67bd5d4ef70b5a57c6985671
tommylees112 <thomas.lees112@gmail.com>
2020-07-28 11:07:44 +0100

    update merge files

--- scripts/drafts/merge_files.py
+++ scripts/drafts/merge_files.py
@@ -7,10 +7,12 @@ from src.preprocess import ERA5LandPreprocessor
 
 
 if __name__ == "__main__":
+    # TODO: generalize for any dataset (use a lookup dict)
     data_dir = get_data_path()
-
-    paths = (data_dir / "interim/reanalysis-era5-land_interim").glob("*.nc")
     processor = ERA5LandPreprocessor(data_dir)
+
+    # get variable and subset_str
+    paths = (data_dir / "interim/reanalysis-era5-land_interim").glob("*.nc")
     subset_str = str(paths[0].name).split("_")[-1].replace(".nc", "")
     variable = str(paths[0].name).split("01_12_")[-1].replace(f"{subset_str}.nc", "")
 
