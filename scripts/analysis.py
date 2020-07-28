7223e9f68d023a4d4c13393ef5b4bb2c7e03cfa5
tommylees112 <thomas.lees112@gmail.com>
2020-07-28 15:44:32 +0100

    update read_pred_dat

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -1,10 +1,18 @@
+import xarray as xr
 from pathlib import Path
+import numpy as np
 import sys
+from typing import (Tuple, List, Union, )
 
 sys.path.append("..")
 
 from src.analysis import AdministrativeRegionAnalysis
 from src.analysis import LandcoverRegionAnalysis
+from scripts.utils import get_data_path
+from src.analysis import read_train_data, read_test_data, read_pred_data
+from src.utils import get_ds_mask
+from src.models import load_model
+from src.models.neural_networks.base import NNBase
 
 
 def run_administrative_region_analysis():
@@ -54,6 +62,65 @@ def run_landcover_region_analysis():
     print(analyzer.regional_mean_metrics)
 
 
+def read_all_data(data_dir: Path, experiment="one_month_forecast", static: bool = False) -> Tuple[xr.Dataset]:
+    X_train, y_train = read_train_data(data_dir, experiment=experiment)
+    X_test, y_test = read_test_data(data_dir, experiment=experiment)
+
+    if static:
+        static_ds = xr.open_dataset(data_dir / "features/static/data.nc")
+    return (X_train, y_train, X_test, y_test)
+
+
+def read_all_available_pred_data(data_dir: Path, experiment: str = "one_month_forecast") -> List[xr.Dataset]:
+    model_dir = (data_dir / f"models/{experiment}")
+    models = np.array([d.name for d in model_dir.iterdir()])
+
+    # drop the models that haven't written .nc files
+    models_run_bool = [
+        any([".nc" in file.name for file in (model_dir / model).iterdir()])
+        for model in models
+    ]
+    models = models[models_run_bool]
+
+    return {model: read_pred_data(model, data_dir, experiment=experiment)[-1] for model in models}
+
+
+def load_nn(data_dir: Path, model_str: str, experiment: str = "one_month_forecast") -> NNBase:
+    return load_model(data_dir / f"models/{experiment}/{model_str}/model.pt")
+
+
+def plot_predictions():
+
+    return
+
+
 if __name__ == "__main__":
-    run_administrative_region_analysis()
-    run_landcover_region_analysis()
+    # run_administrative_region_analysis()
+    # run_landcover_region_analysis()
+    data_dir = get_data_path()
+    experiment = "one_month_forecast"
+
+    # read train/test data
+    X_train, y_train, X_test, y_test = read_all_data(
+        data_dir,
+        static=False,
+        experiment=experiment,
+    )
+    mask = get_ds_mask(X_train.VCI)
+
+    # read the predicted data
+    predictions = read_all_available_pred_data(
+        data_dir,
+        experiment=experiment,
+    )
+
+    # read in the models (pytorch models)
+    models_str = [m for m in predictions.keys()]
+    models = {}
+    for m in [m for m in models_str if m != "previous_month"]:
+        models[m] = load_nn(data_dir, m, experiment=experiment)
+
+
+preds = predictions['rnn']
+model = 'rnn'
+read_pred_data(model, data_dir, experiment=experiment)
