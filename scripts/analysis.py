e86db75fc0fca54bfa2d41e53f8f6546357b0532
tommylees112 <thomas.lees112@gmail.com>
2020-07-29 09:36:14 +0100

    update analysis script

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -2,7 +2,7 @@ import xarray as xr
 from pathlib import Path
 import numpy as np
 import sys
-from typing import (Tuple, List, Union, )
+from typing import (Tuple, List, Union, Dict)
 
 sys.path.append("..")
 
@@ -13,6 +13,8 @@ from src.analysis import read_train_data, read_test_data, read_pred_data
 from src.utils import get_ds_mask
 from src.models import load_model
 from src.models.neural_networks.base import NNBase
+from src.analysis import spatial_rmse, spatial_r2
+
 
 
 def run_administrative_region_analysis():
@@ -89,6 +91,31 @@ def load_nn(data_dir: Path, model_str: str, experiment: str = "one_month_forecas
     return load_model(data_dir / f"models/{experiment}/{model_str}/model.pt")
 
 
+def create_all_error_metrics(
+    predictions: Dict[str, xr.DataArray],
+    y_test: xr.DataArray
+) -> Tuple[Dict[str, xr.DataArray]]:
+    rmse_dict = {}
+    r2_dict = {}
+
+    for model in [m for m in predictions.keys()]:
+        model_preds = predictions[model]
+        model_rmse = spatial_rmse(
+            y_test.transpose("time", "lat", "lon"),
+            model_preds.transpose("time", "lat", "lon"),
+        )
+        model_rmse.name = "rmse"
+        rmse_dict[model] = model_rmse
+        model_r2 = spatial_r2(
+            y_test.transpose("time", "lat", "lon"),
+            model_preds.transpose("time", "lat", "lon"),
+        )
+        model_r2.name = "r2"
+        r2_dict[model] = model_r2
+
+    return rmse_dict, r2_dict
+
+
 def plot_predictions():
 
     return
@@ -97,9 +124,13 @@ def plot_predictions():
 if __name__ == "__main__":
     # run_administrative_region_analysis()
     # run_landcover_region_analysis()
+
     data_dir = get_data_path()
     experiment = "one_month_forecast"
 
+    # ---------------------------------------- # 
+    # Read in the data # 
+    # ---------------------------------------- # 
     # read train/test data
     X_train, y_train, X_test, y_test = read_all_data(
         data_dir,
@@ -107,12 +138,19 @@ if __name__ == "__main__":
         experiment=experiment,
     )
     mask = get_ds_mask(X_train.VCI)
+    test_da = y_test[list(y_test.data_vars)[0]]
 
     # read the predicted data
     predictions = read_all_available_pred_data(
         data_dir,
         experiment=experiment,
     )
+    # check that the shapes of all predictions are the same
+    assert set([predictions[m].shape for m in predictions.keys()]).__len__() == 1
+    preds = predictions[list(m for m in predictions.keys())[0]]
+
+    # get the matching shapes from predictions and test data
+    test_da = test_da.sel(lat=preds.lat, lon=preds.lon, time=preds.time)
 
     # read in the models (pytorch models)
     models_str = [m for m in predictions.keys()]
@@ -120,7 +158,16 @@ if __name__ == "__main__":
     for m in [m for m in models_str if m != "previous_month"]:
         models[m] = load_nn(data_dir, m, experiment=experiment)
 
+    # Calculate error metrics
+    rmse_dict, r2_dict = create_all_error_metrics(predictions, test_da)
 
-preds = predictions['rnn']
-model = 'rnn'
-read_pred_data(model, data_dir, experiment=experiment)
+    # ---------------------------------------- # 
+    # Make the plots # 
+    # ---------------------------------------- # 
+    # 1. Spatial Plots of RMSE
+    for m in models_str:
+        fig, ax = plt.subplots()
+        # fig.savefig()
+    # 1. Spatial Plots of R2
+    # 1. Scatter Plots of Observed vs. Predicted
+    # 1. Confusion matrix of the Vegetation Deficit Index scores
