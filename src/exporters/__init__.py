7a778be0472e5d11e9081b9f1b566aa9c1b15b54
tommylees112 <thomas.lees112@gmail.com>
2020-08-07 11:12:22 +0100

    update

--- src/exporters/__init__.py
+++ src/exporters/__init__.py
@@ -4,7 +4,7 @@ from .chirps import CHIRPSExporter
 from .planetOS import ERA5ExporterPOS
 from .seas5.s5 import S5Exporter
 from .gleam import GLEAMExporter
-from .era5_land import ERA5LandExporter
+from .era5_land import ERA5LandExporter, ERA5LandExporterHourly
 from .srtm import SRTMExporter
 from .esa_cci import ESACCIExporter
 from .admin_boundaries import KenyaAdminExporter
@@ -19,7 +19,8 @@ __all__ = [
     "GLEAMExporter",
     "SRTMExporter",
     "ESACCIExporter",
-    "ERA5LandExporter",
     "KenyaAdminExporter",
     "BokuNDVIExporter",
+    "ERA5LandExporter",
+    "ERA5LandExporterHourly",
 ]
