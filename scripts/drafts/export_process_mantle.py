c3d5ab257ef95fe68f4d46c8e9f70d7431b3851f
tommylees112 <thomas.lees112@gmail.com>
2020-10-13 19:41:53 +0100

    regird path

--- scripts/drafts/export_process_mantle.py
+++ scripts/drafts/export_process_mantle.py
@@ -25,7 +25,7 @@ def preprocess_mantle_modis(subset_str: str = "india"):
     processor = MantleModisPreprocessor(data_path)
     #  upsampling from low -> high resolution
     processor.preprocess(
-        subset_str=subset_str, regrid=regrid_path, upsampling=False,
+        subset_str=subset_str, regrid_path=regrid_path, upsampling=False,
     )
 
 
