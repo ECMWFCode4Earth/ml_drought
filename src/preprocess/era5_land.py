be1680abb4966abf8b75721a8d7d8488f0ee0344
tommylees112 <thomas.lees112@gmail.com>
2020-10-15 14:10:19 +0100

    fix bug in era5 land

--- src/preprocess/era5_land.py
+++ src/preprocess/era5_land.py
@@ -114,7 +114,7 @@ class ERA5LandPreprocessor(BasePreProcessor):
         nc_files = self.get_filepaths()
         if years is not None:
             nc_files = [
-                f for f in nc_files if int(f.parents[0]) in years  # type: ignore
+                f for f in nc_files if int(f.parents[0].name) in years  # type: ignore
             ]
 
         # run for one variable or all variables?
