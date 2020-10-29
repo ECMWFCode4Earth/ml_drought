b6d83a1d55d27d085bb21eaff74d060b4c496cd3
tommylees112 <thomas.lees112@gmail.com>
2020-10-29 15:25:42 +0000

    draft script

--- scripts/drafts/cut_basins_from_soil_moisture.py
+++ scripts/drafts/cut_basins_from_soil_moisture.py
@@ -5,6 +5,7 @@ import pandas as pd
 from pathlib import Path
 import xarray as xr
 from scripts.utils import get_data_path
+from src.preprocess.utils import SHPtoXarray
 
 
 if __name__ == "__main__":
@@ -13,6 +14,7 @@ if __name__ == "__main__":
     sm_path = data_dir / "RUNOFF/gb_soil_moisture_2000_2020.nc"
     shp_path = data_dir / "CAMELS_GB_DATASET/Catchment_Boundaries/CAMELS_GB_catchment_boundaries.shp"
 
-    ds = xr.open_dataset(sm_path)
-    gr = GroupbyRegion(data_dir, country=shp_path.as_posix())
+    da = xr.open_dataset(sm_path)["swvl1"]
 
+    converter = SHPtoXarray()
+    shp_xr = converter.shapefile_to_xarray(da, shp_path, var_name="station_id", lookup_colname="ID_STRING")
