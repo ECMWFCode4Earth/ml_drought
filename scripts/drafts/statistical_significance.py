abd5e22fe8ed26a436cbc1a4ef31dc45eaca16d5
tommylees112 <thomas.lees112@gmail.com>
2021-01-12 19:10:47 +0000

    add tsts

--- scripts/drafts/statistical_significance.py
+++ scripts/drafts/statistical_significance.py
@@ -44,4 +44,5 @@ if __name__ == "__main__":
     data_dir = Path("/cats/datastore/data/")
     all_metrics = pickle.load((data_dir / "RUNOFF/all_metrics.pkl").open("rb"))
 
-    print(run_test(all_metrics, metric="nse", ref_model="LSTM"))
+    print(run_test(all_metrics, test="ks", metric="nse", ref_model="LSTM"))
+    print(run_test(all_metrics, test="wilcoxon", metric="nse", ref_model="LSTM"))
