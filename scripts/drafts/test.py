e275660eb9ff9737be6f9ca25694147efa0e0717
tommylees112 <thomas.lees112@gmail.com>
2021-01-15 12:18:10 +0000

    run seasonal metrics

--- scripts/drafts/test.py
+++ scripts/drafts/test.py
@@ -10,3 +10,31 @@ if __name__ == "__main__":
     test_model = [k for k in all_errors.keys()][0]
     test_metric = [k for k in all_metrics.keys()][0]
     assert all(all_errors[test_model][test_metric].dropna() == all_metrics[test_metric][test_model].dropna())
+
+    ##
+    import xarray as xr
+    import sys
+    sys.path.append()
+    sys.path.append("/home/tommy/ml_drought")
+    from scripts.drafts.calculate_error_scores import calculate_seasonal_errors, get_metric_dataframes_from_output_dict
+    from collections import defaultdict
+
+    save = True
+    all_preds = xr.open_dataset()
+    seasonal_errors = calculate_seasonal_errors(all_preds)
+    #  calculate seasonal metrics
+    seasonal_metrics = defaultdict(dict)
+    for season in ["DJF", "MAM", "JJA", "SON"]:
+        seasonal_metrics[season] = get_metric_dataframes_from_output_dict(
+            seasonal_errors[season]
+        )
+
+    if save:
+        import pickle
+
+        pickle.dump(
+            seasonal_errors, (data_dir / "RUNOFF/seasonal_errors.pkl").open("wb")
+        )
+        pickle.dump(
+            seasonal_metrics, (data_dir / "RUNOFF/seasonal_metrics.pkl").open("wb")
+        )
