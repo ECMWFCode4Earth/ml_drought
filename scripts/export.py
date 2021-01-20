c638bd38939faf3d5af53f1d296017a64b16c63f
tommylees112 <thomas.lees112@gmail.com>
2021-01-20 13:20:57 +0000

    export script

--- scripts/export.py
+++ scripts/export.py
@@ -190,7 +190,7 @@ def export_boku_ndvi():
 
 if __name__ == "__main__":
     print(f"Writing data to: {get_data_path()}")
-    export_era5_land(region_str="india")
+    # export_era5_land(region_str="india")
     # export_era5(region_str="kenya")
     # export_vhi()
     # export_chirps()
@@ -199,3 +199,4 @@ if __name__ == "__main__":
     # export_esa()
     # export_s5(region_str="kenya")
     # export_kenya_boundaries()
+    export_boku_ndvi()
